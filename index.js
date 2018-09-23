2ed6a6fe65ca89d72e88c64cc74ca73c6d60024a
Kovacs Erik <kovaroberik@gmail.com>
2018-09-23 17:33:04 +0300

    done some additional setup for heroku and fb

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ server.post('/webhook', (req, res) => {
 // Adds support for GET requests to our webhook
 server.get('/webhook', (req, res) => {
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
+    let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
         
     // Parse the query params
     let mode = req.query['hub.mode'];
