bb63868931bbec29d1624329a000198da48f43e3
Kovacs <ekovacs@techteam.com>
2018-10-22 13:40:27 +0300

    testing

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function callSendAPI(sender_psid, response) {
   // Send the HTTP request to the Messenger Platform
   request(
     {
-      uri: "https://graph.facebook.com/v2.6/me/messages",
+      uri: "https://graph.facebook.com/v3.1/me/messages",
       qs: {
         access_token: PAGE_ACCESS_TOKEN
       },
