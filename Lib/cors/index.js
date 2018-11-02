0817175f479bd426549caba280b44d67f2ac7f16
Kovacs Erik-Robert <kovaroberik@gmail.com>
2018-11-02 17:40:45 +0200

    cors

--- Lib/cors/index.js
+++ Lib/cors/index.js
@@ -42,6 +42,10 @@ const cors = (req, res, next) => {
           "Access-Control-Max-Age": "1728000"
         },
         {
+          "Access-Control-Allow-Headers":
+            "Access-Control-Allow-Headers, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+        },
+        {
           "Content-Length": "0"
         },
         {
