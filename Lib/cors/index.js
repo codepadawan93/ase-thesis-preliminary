d5cc44f6dd26764ef076ad98d6509695db6cb55a
Kovacs Erik-Robert <kovaroberik@gmail.com>
2018-11-02 17:41:35 +0200

    cors

--- Lib/cors/index.js
+++ Lib/cors/index.js
@@ -43,7 +43,7 @@ const cors = (req, res, next) => {
         },
         {
           "Access-Control-Allow-Headers":
-            "Access-Control-Allow-Headers, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            "Access-Control-Allow-Headers, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
         },
         {
           "Content-Length": "0"
