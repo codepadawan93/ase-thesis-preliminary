ad41526b9999b923a8aeb1032c28bf8ac7452c0e
Kovacs Erik-Robert <kovaroberik@gmail.com>
2018-11-02 17:15:57 +0200

    added proper check before 403

--- Lib/cors/index.js
+++ Lib/cors/index.js
@@ -3,6 +3,9 @@ const HTTP = require("./enums/http");
 const cors = (req, res, next) => {
   let headers = [];
   let code = 200;
+
+  const origin = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
   if (req.method === HTTP.GET) {
     headers = [
       ...headers,
@@ -23,28 +26,31 @@ const cors = (req, res, next) => {
       }
     ];
   } else if (req.method === HTTP.OPTIONS) {
-    headers = [
-      ...headers,
-      {
-        "Access-Control-Allow-Origin": process.env.ALLOWED_DOMAINS
-      },
-      {
-        "Access-Control-Allow-Methods": "GET, OPTIONS"
-      },
-      {
-        "Access-Control-Allow-Credentials": "true"
-      },
-      {
-        "Access-Control-Max-Age": "1728000"
-      },
-      {
-        "Content-Length": "0"
-      },
-      {
-        "Content-Type": "text/plain"
-      }
-    ];
-    code = 403;
+    if (process.env.ALLOWED_DOMAINS.split(", ").indexOf(origin) !== -1) {
+      headers = [
+        ...headers,
+        {
+          "Access-Control-Allow-Origin": process.env.ALLOWED_DOMAINS
+        },
+        {
+          "Access-Control-Allow-Methods": "GET, OPTIONS"
+        },
+        {
+          "Access-Control-Allow-Credentials": "true"
+        },
+        {
+          "Access-Control-Max-Age": "1728000"
+        },
+        {
+          "Content-Length": "0"
+        },
+        {
+          "Content-Type": "text/plain"
+        }
+      ];
+    } else {
+      code = 403;
+    }
   } else {
     headers = [...headers, { "Content-Type": "text/plain" }];
     code = 400;
