94495c43e48dad0bb905d16c4ca15aad8ba4a712
Kovacs Erik <kovaroberik@gmail.com>
2019-03-02 23:51:23 +0200

    added build folder to commit

---
+++ Static/build/precache-manifest.ad6e536df80411317a2460a3341dbaa5.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "b78d5b2c4140051adae8",
+    "url": "/static/css/main.605d4311.chunk.css"
+  },
+  {
+    "revision": "69c960b659c7170d9ae14832e3ba0482",
+    "url": "/static/media/bg-masthead.69c960b6.png"
+  },
+  {
+    "revision": "4222810c104fdcf8f66d",
+    "url": "/static/js/1.4222810c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fa58956ab656eb4098b93767b444c0e",
+    "url": "/static/media/your_next_vacation.1fa58956.png"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "b78d5b2c4140051adae8",
+    "url": "/static/js/main.b78d5b2c.chunk.js"
+  },
+  {
+    "revision": "6d4774d4e483e03057f9d09544656b42",
+    "url": "/static/media/fa-regular-400.6d4774d4.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "3897e68864e7fe1fbb468c0220573010",
+    "url": "/static/media/fa-brands-400.3897e688.svg"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "3fad11457f70f880252e0ab5c9cee82e",
+    "url": "/static/media/fa-solid-900.3fad1145.svg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "73a932562a1e314703469d0a352fcda9",
+    "url": "/static/media/Simple-Line-Icons.73a93256.svg"
+  },
+  {
+    "revision": "4222810c104fdcf8f66d",
+    "url": "/static/css/1.4c0a97f1.chunk.css"
+  },
+  {
+    "revision": "7fb418a28e051d4af3f81a18e5228671",
+    "url": "/index.html"
+  }
+];
