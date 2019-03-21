05a2a58f2d785758e540310d895d9fdabb8e8ad0
--global <kovaroberik@gmail.com>
2019-03-21 19:16:07 +0200

    added firebase integration

--- Library/cred-manager.js
+++ Library/cred-manager.js
@@ -4,6 +4,9 @@ module.exports = class CredManager {
   static SERVICE_ACCOUNT_JSON_PATH() {
     return "./Config/serviceAccountKey.json";
   }
+  static CONFIG_JSON_PATH() {
+    return "./Static/src/config/config.json";
+  }
 
   static createServiceAccountJson() {
     if (fs.existsSync(CredManager.SERVICE_ACCOUNT_JSON_PATH())) {
@@ -13,6 +16,21 @@ module.exports = class CredManager {
     if (jsonBase64) {
       const json = new Buffer(jsonBase64, "base64").toString("ascii");
       fs.writeFileSync(CredManager.SERVICE_ACCOUNT_JSON_PATH(), json, err => {
+        if (err) console.error(err);
+      });
+    } else {
+      console.error("No credentials set.");
+    }
+  }
+
+  static createConfigJson() {
+    if (fs.existsSync(CredManager.CONFIG_JSON_PATH())) {
+      return;
+    }
+    const jsonBase64 = process.env.CONFIG_JSON;
+    if (jsonBase64) {
+      const json = new Buffer(jsonBase64, "base64").toString("ascii");
+      fs.writeFileSync(CredManager.CONFIG_JSON_PATH(), json, err => {
         if (err) console.error(err);
       });
     } else {
